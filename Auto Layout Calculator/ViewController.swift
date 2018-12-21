b4a9e7d244f1178f0962d39f2652de35f3e3b606
Muhammad Ewaily <muhammadewaily@Ewailys-Mac.local>
2018-12-21 17:58:50 +0200

    Complete UI

---
+++ Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by Muhammad Ewaily on 12/19/18.
+//  Copyright © 2018 Muhammad Ewaily. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
